731cd12d5fa869458ed276c57e65dd1c73627e1b
Abuzzpoet <76829545+Abuzzpoet@users.noreply.github.com>
2022-09-24 17:07:03 +0700

    Akame V2.0

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 
 require('./config')
 const { default: hisokaConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
-const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
+const { state, saveState } = useSingleFileAuthState(`./${global.sessionName}.json`)
 const pino = require('pino')
 const { Boom } = require('@hapi/boom')
 const fs = require('fs')
