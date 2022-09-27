7d7b9a291fd2580891691e6cfe6d0904713c881b
Abuzzpoet <76829545+Abuzzpoet@users.noreply.github.com>
2022-09-27 16:49:31 +0700

    Akame V2.0

--- hisoka.js
+++ hisoka.js
@@ -2811,7 +2811,7 @@ ${vote[m.chat][2].map((v, i) => `├ ${i + 1}. @${v.split`@`[0]}`).join('\n')}
             atas = text.split('|')[0] ? text.split('|')[0] : '-'
             bawah = text.split('|')[1] ? text.split('|')[1] : '-'
 	        let dwnld = await hisoka.downloadMediaMessage(qmsg)
-	        let { TelegraPh } = require('./Lib/uploader')
+	        let { TelegraPh } = require('./lib/uploader')
 	        let fatGans = await TelegraPh(dwnld)
 	        let smeme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas)}/${encodeURIComponent(bawah)}.png?background=${fatGans}`
 	        let FaTiH = await hisoka.sendImageAsSticker(m.chat, smeme, m, { packname: global.packname, author: global.auhor })
@@ -3850,7 +3850,7 @@ ${vote[m.chat][2].map((v, i) => `├ ${i + 1}. @${v.split`@`[0]}`).join('\n')}
                 if (!/image/.test(mime)) return m.reply(`Kirim/Reply Foto`)
                 m.reply(mess.wait)
                 let dwnld = await hisoka.downloadMediaMessage(qmsg)
-                let { TelegraPh } = require('./Lib/uploader')
+                let { TelegraPh } = require('./lib/uploader')
                 let fatGans = await TelegraPh(dwnld)
                 let smeme = api('zenz', '/photoeditor/' + command, { url: fatGans }, 'apikey')
                 let FaTiH = await hisoka.sendImageAsSticker(m.chat, smeme, m, {
