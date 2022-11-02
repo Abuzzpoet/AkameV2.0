6c76d19d4e624002baf31073f1e143e72d3e965f
Abuzzpoet <76829545+Abuzzpoet@users.noreply.github.com>
2022-11-02 15:29:52 +0700

    Akame V2.0
    Signed-off-by: Abuzzpoet <76829545+Abuzzpoet@users.noreply.github.com>

--- akame.js
+++ akame.js
@@ -26,7 +26,7 @@ const speed = require('performance-now')
 const { performance } = require('perf_hooks')
 const { Primbon } = require('scrape-primbon')
 const primbon = new Primbon()
-const { smsg, formatp, tanggal, formatDate, getTime, isUrl, sleep, clockString, runtime, fetchJson, getBuffer, jsonformat, format, parseMention, getRandom, getGroupAdmins, generateProfilePicture, reSize } = require('./lib/myfunc')
+const { smsg, getGroupAdmins, formatp, tanggal, formatDate, getTime, isUrl, await, sleep, clockString, msToDate, sort, toNumber, enumGetKey, runtime, fetchJson, getBuffer, jsonformat, delay, format, logic, generateProfilePicture, parseMention, getRandom, pickRandom} = require('../lib/myfunc')
 
 // read database
 global.db.data = JSON.parse(fs.readFileSync('./src/database.json'))
